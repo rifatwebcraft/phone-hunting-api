51b83f75ba25cca10f4fc146ce05aa3555c40fff
rifatwebcraft <cmohammadrifat@gmail.com>
2024-03-02 16:25:24 +0600

    something edited

--- js/phone.js
+++ js/phone.js
@@ -2,7 +2,15 @@ const loadPhone = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
     const data = await res.json();
     const phones = data.data;
-    console.log(phones);
+    // console.log(phones);
+    displayPhone(phones);
+}
+
+function displayPhone(phones){
+    // console.log(phones)
+    phones.forEach(phones => {
+        console.log(phones);
+    });
 }
 
 loadPhone();
