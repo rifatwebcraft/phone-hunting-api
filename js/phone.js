2379b45cbf6f27d1ac86ee530ca35698b68644da
rifatwebcraft <cmohammadrifat@gmail.com>
2024-03-03 00:36:39 +0600

    try to finish

--- js/phone.js
+++ js/phone.js
@@ -1,16 +1,83 @@
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchValue, isShowAll) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchValue}`);
     const data = await res.json();
     const phones = data.data;
     // console.log(phones);
-    displayPhone(phones);
+    displayPhone(phones, isShowAll);
 }
 
-function displayPhone(phones){
+const displayPhone = (phones, isShowAll) =>{
     // console.log(phones)
+    //1. get container
+    const phoneContainer = document.getElementById('phone-container');
+    //clear the container cards before adding new card
+    phoneContainer.textContent = ' '; //note : aikane container k clear kora hoyeche new search result dekanor joono..
+    //search result phones length check...
+    console.log(phones.length);
+    //display show all button if there are more than 12 phones..
+    const showAllContainer = document.getElementById('show-all-container')
+    if(phones.length > 12 && !isShowAll){
+        showAllContainer.classList.remove('hidden');
+    }
+    else{
+        showAllContainer.classList.add('hidden')
+    }
+    //display only first 12 phones..if not showall
+    
+    if(!isShowAll){
+        phones = phones.slice(0,12);
+    }
+
     phones.forEach(phones => {
         console.log(phones);
+        //2. creat a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card bg-base-100 shadow-xl p-7`;
+        //3.set innerHTML
+        phoneCard.innerHTML = `
+        <figure><img src="${phones.image}" alt="Shoes" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">${phones.phone_name}</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-center">
+            <button onclick="handleShowDetail('${phones.slug}')" class="btn btn-primary">Show Details</button>
+          </div>
+        </div>
+        `
+         //4. appndChild..
+        phoneContainer.appendChild(phoneCard);
     });
+    //hide loading spinner
+    toggleLoadingSpinner(false);
 }
 
-loadPhone();
+//
+const handleShowDetail = (id) =>{
+    console.log('gumai ja', id);
+}
+
+//handle search button
+const handleSearch = (isShowAll) => {
+    const searchField = document.getElementById('input-field');
+    const searchValue = searchField.value;
+    console.log(searchValue);
+    loadPhone(searchValue, isShowAll);
+    toggleLoadingSpinner(true);
+}
+
+
+const toggleLoadingSpinner = (isLoading) => {
+    const loadingSpinner = document.getElementById('loading-spinner');
+    if(isLoading){
+        loadingSpinner.classList.remove('hidden');
+    }
+    else{
+        loadingSpinner.classList.add('hidden');
+    }
+}
+
+
+//handle show all button
+const handleShowAll = () =>{
+    handleSearch(true);
+}
